35285f3ca1cf276444b07a1512189369b5f02fa8
Anupamdatta10 <anupamdatta10@gmail.com>
2021-11-10 09:22:15 +0530

    search

--- index.js
+++ index.js
@@ -49,16 +49,28 @@ app.get('/delete/:id',function(req,res){
     res.redirect("/")
     })
 })
-// app.get('/data/:id', function (req, res) {
-//     var x=req.params.id;
-//     console.log(x)
-//     res.render("home",{name:["anupam","nirveek","sourav"],x:x})
-
-// })
-// app.post('/form',function(req,res){
-// var data=req.body.name;
-//     res.render("home",{name:[data,"nirveek","sourav"],x:"not foun"})
-// })
+app.get('/update/:id',function(req,res){
+    connection.query('Update To_do set status=? where T_id=?',["Done",req.params.id],(err,result,fields)=>{
+        console.log(result)
+    res.redirect("/")
+    })
+})
+app.get('/getDone',function(req,res){
+    connection.query('select * from to_do where status="Done"',(error,result,feilds)=>{
+        res.render("home",{data:result})
+    })
+})
+app.get('/getToDo',function(req,res){
+    connection.query('select * from to_do where status="To_Do"',(error,result,feilds)=>{
+        res.render("home",{data:result})
+    })
+})
+app.get('/search/:id',function(req,res){
+    connection.query('select * from to_do where T_Name like ("%'+req.params.id+'%")',(error,result,feilds)=>{
+        console.log(result)
+        res.render("home",{data:result})
+    })
+})
 app.listen(3000,()=>{
     console.log("http://localhost:3000")
 })
